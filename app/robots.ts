cf93b8f8e31cacd331203665f02d69899ea4949c
yangyuanchun268791 <yangyuanchun268791@users.noreply.github.com>
2026-05-14 22:30:03 +0800

    Add: sitemap.xml, robots.txt, fix lockfile warning

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/admin/'],
+    },
+    sitemap: 'https://healthroute.xyz/sitemap.xml',
+    host: 'https://healthroute.xyz',
+  }
+}
